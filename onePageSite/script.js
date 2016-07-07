36b75424539626522a66d69ce3088441d065bf1a
CEREBR4L <mattmillergraphics@gmail.com>
2016-07-07 19:14:56 +0100

    Angularing

---
+++ onePageSite/script.js
@@ -0,0 +1,29 @@
+var scotchApp = angular.module('scotchApp', []);
+
+//Routing...
+scotchApp.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
+		.when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController'
+		});
+});
+
+scotchApp.controller('mainController', function($scope){
+	$scope.message = 'Everyone come and look at me!';
+});
+
+scotchApp.controller('aboutController', function($scope){
+	$scope.message = "Look, an about page!";
+});
+scotchApp.controller('contactController', function($scope){
+	$scope.message = "Look, a contact page!";
+});
